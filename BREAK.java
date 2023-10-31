dd2d268d184fb08a0f8fa523f4295285487150a6
adeline-ishimwe <149521128+adeline-ishimwe@users.noreply.github.com>
2023-10-31 19:35:58 +0200

    Add files via upload

---
+++ BREAK.java
@@ -0,0 +1,10 @@
+public class BREAK {
+    public static void main(String[] args) {  
+        for(int i=2;i<=10;i++){  
+            if(i==5){  
+                break;  
+            }  
+            System.out.println(i);  
+        }  
+    }  
+    }  

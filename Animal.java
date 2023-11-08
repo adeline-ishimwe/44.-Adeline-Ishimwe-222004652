6c628fad862a4c609cad16fa6517604305cbdb3d
adeline-ishimwe <149521128+adeline-ishimwe@users.noreply.github.com>
2023-11-08 20:06:21 +0200

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,42 @@
+public Animal(String name){
+        this.name = name;
+    }
+
+    public void speak() {
+        System.out.println(name + " makes a sound.");
+    }
+
+    public void eat() {
+        System.out.println(name + " is eating.");
+    }
+}
+
+
+class Dog extends Animal {
+    public Dog(String name) {
+        super(name); // Call the superclass constructor
+    }
+
+    public void speak() {
+        System.out.println( " barks!");
+    }
+
+    public void fetch() {
+        System.out.println( "is fetching a ball.");
+    }
+  
+    public static void main(String[] args) {
+        Animal animal = new Animal("Generic Animal");
+        Dog dog = new Dog("Buddy");
+
+        // Call methods on the objects
+        animal.speak(); // Output: Generic Animal makes a sound.
+        animal.eat();   // Output: Generic Animal is eating.
+
+        dog.speak();     // Output: Buddy barks!
+        dog.eat();       // Output: Buddy is eating.
+        dog.fetch();     // Output: Buddy is fetching a ball.
+
+    }
+
+    } 

75e56a3de09525c8de18d5ea83dc25de04d463be
adeline-ishimwe <149521128+adeline-ishimwe@users.noreply.github.com>
2023-11-08 21:20:07 +0200

    Add files via upload

--- Animal.java
+++ Animal.java
@@ -1,6 +1,11 @@
-public Animal(String name){
+ class Animal{ 
+    private String name;
+
+    public Animal(String name){
         this.name = name;
-    }
+ 
+ 
+}
 
     public void speak() {
         System.out.println(name + " makes a sound.");
